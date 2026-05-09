d3c646af481979958cc137a30de4890e076cc011
Bhagvan Pavan <bhagavanpavan999@gmail.com>
2026-05-09 20:25:34 +0530

    This is my first Problem

---
+++ 01.py
@@ -0,0 +1,9 @@
+print("Jai Hanuman")
+
+nums = [0,0,1,2,3,6,4,0,0,6,0,7,8,0]
+j = 0
+for i in range(len(nums)):
+    if nums[j] > nums[i] :
+        nums[i],nums[j] = nums[j],nums[i]
+        j = j + 1
+print(nums)
